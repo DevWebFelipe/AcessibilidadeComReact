fd14e75896a48341c081a5670f4004843ddfd0b7
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-08 19:11:42 -0300

    alterado forma de executar o axe-core para usar useEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter"
-
-axeAccessibilityReporter()
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter()
+  }, [])
+
   return <Component {...pageProps} />
 }
 
--- src/utils/axeAccessibilityReporter.ts
+++ src/utils/axeAccessibilityReporter.ts
@@ -1,5 +1,5 @@
 export async function axeAccessibilityReporter() {
-  if (process.env.NODE_ENV == "development" && typeof window !== "undefined") {
+  if (process.env.NODE_ENV == "development") {
     const axe = (await import("@axe-core/react")).default
     const React = (await import("react")).default
     const ReactDOM = (await import("react-dom")).default
