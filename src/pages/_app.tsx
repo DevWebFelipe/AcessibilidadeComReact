f849def988f47618067fb9de5e6a9d89027c5547
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-08 19:07:25 -0300

    instalado axe-core diretamente no projeto em modo de desenvolvimento

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter"
+
+axeAccessibilityReporter()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/utils/axeAccessibilityReporter.ts
@@ -0,0 +1,9 @@
+export async function axeAccessibilityReporter() {
+  if (process.env.NODE_ENV == "development" && typeof window !== "undefined") {
+    const axe = (await import("@axe-core/react")).default
+    const React = (await import("react")).default
+    const ReactDOM = (await import("react-dom")).default
+
+    axe(React, ReactDOM, 1000)
+  }
+}
